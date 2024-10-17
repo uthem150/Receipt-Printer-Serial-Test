f0eb45050949b1a2c2659afeb57ec38f040853fd
uthem150 <uthem150@gmail.com>
2024-10-17 10:01:13 +0900

    feat: 한글은 2배의 너비를 가지는 것을 고려, 문자열 길이 조정

--- serial-test/src/function/printReceipt.js
+++ serial-test/src/function/printReceipt.js
@@ -3,6 +3,15 @@ import iconv from "iconv-lite";
 // ms 밀리초가 지나면 Promise가 해결(resolve)됨
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
+function getKorCount(str) {
+  let length = 0; // 총 길이를 저장할 변수 초기화
+  for (let char of str) {
+    // 문자열의 각 문자에 대해 반복
+    length += char.charCodeAt(0) > 127 ? 1 : 0; // 유니코드 값이 127보다 크면, 해당 문자는 2바이트(한글)로 간주하고, 그렇지 않으면 (영어, 숫자 등)
+  }
+  return length; // 총 길이 반환
+}
+
 export const createReceiptTemplate = async (info) => {
   const {
     hotelName,
@@ -71,7 +80,7 @@ export const createReceiptTemplate = async (info) => {
     // 표 헤더 출력
     await writeAndWait(
       iconv.encode(
-        "상 품 명               단 가     수량     금  액\n",
+        "       상 품 명          단 가    수량     금 액 \n", // 7,8,10,5,3,4,5,5,2
         "cp949"
       )
     );
@@ -80,15 +89,19 @@ export const createReceiptTemplate = async (info) => {
 
     const itemsText = items
       .map((item) => {
-        const itemName = item.name.padEnd(20); // 상품 이름 정렬
-        const itemPrice = item.price.toLocaleString().padStart(10); // 가격 정렬
-        const itemQuantity = item.quantity.toLocaleString().padStart(10); // 수량 정렬
+        const itemNameLength = 22; // 원하는 총 길이
+        const itemName = item.name.padEnd(
+          itemNameLength - getKorCount(item.name)
+        ); // 상품 이름 정렬
+
+        const itemPrice = item.price.toLocaleString().padStart(8); // 가격 정렬
+        const itemQuantity = item.quantity.toLocaleString().padStart(7); // 수량 정렬
         const itemTotal = (item.price * item.quantity)
           .toLocaleString()
-          .padStart(10); // 총액 정렬
+          .padStart(12); // 총액 정렬
 
         // 포맷팅된 상품 정보
-        return `${itemName} ${itemPrice} ${itemQuantity} ${itemTotal}\n`;
+        return `${itemName}${itemPrice}${itemQuantity}${itemTotal}`;
       })
       .join("\n");
     await writeAndWait(iconv.encode(itemsText, "cp949"));
